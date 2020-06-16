903b18d47a1db885e9925417ec5d592735c21c9a
Satyaki <36334099+bLACKZU@users.noreply.github.com>
2020-06-16 14:09:28 +0530

    Update

--- SoundSynth/SoundSynth/src/main.cpp
+++ SoundSynth/SoundSynth/src/main.cpp
@@ -3,37 +3,205 @@
 #include <Windows.h>
 #include "noiseMaker.h"
 
+using namespace std;
+
+
+double freqToAngVel(double hertz)
+{
+	return hertz * 2.0 * PI;
+}
+
+// General purpose oscillator
+#define OSC_SINE 0
+#define OSC_SQUARE 1
+#define OSC_TRIANGLE 2
+#define OSC_SAW_ANA 3
+#define OSC_SAW_DIG 4
+#define OSC_NOISE 5
+
+double osc(double hertz, double time, int nType = OSC_SINE)
+{
+	switch (nType)
+	{
+	case OSC_SINE: // Sine wave bewteen 
+		return sin(freqToAngVel(hertz) * time);
+
+	case OSC_SQUARE: // Square wave between 
+		return sin(freqToAngVel(hertz) * time) > 0 ? 1.0 : -1.0;
+
+	case OSC_TRIANGLE: // Triangle wave between -1 and +1
+		return asin(sin(freqToAngVel(hertz) * time)) * (2.0 / PI);
+
+	case OSC_SAW_ANA: // Saw wave (analogue / warm / slow)
+	{
+		double dOutput = 0.0;
+
+		for (double n = 1.0; n < 40.0; n++)
+			dOutput += (sin(n * freqToAngVel(hertz) * time)) / n;
+
+		return dOutput * (2.0 / PI);
+	}
+
+	case OSC_SAW_DIG: // Saw Wave (optimised / harsh / fast)
+		return (2.0 / PI) * (hertz * PI * fmod(time, 1.0 / hertz) - (PI / 2.0));
+
+	default:
+		return 0.0;
+	}
+}
+
+struct sEnvelopeADSR
+{
+	double dAttackTime;
+	double dDecayTime;
+	double dSustainAmplitude;
+	double dReleaseTime;
+	double dStartAmplitude;
+	double dTriggerOffTime;
+	double dTriggerOnTime;
+	bool bNoteOn;
+
+	sEnvelopeADSR()
+	{
+		dAttackTime = 0.10;
+		dDecayTime = 0.01;
+		dStartAmplitude = 1.0;
+		dSustainAmplitude = 0.8;
+		dReleaseTime = 0.20;
+		bNoteOn = false;
+		dTriggerOffTime = 0.0;
+		dTriggerOnTime = 0.0;
+	}
+
+	// Call when key is pressed
+	void NoteOn(double timeOn)
+	{
+		dTriggerOnTime = timeOn;
+		bNoteOn = true;
+	}
+
+	// Call when key is released
+	void NoteOff(double timeOff)
+	{
+		dTriggerOffTime = timeOff;
+		bNoteOn = false;
+	}
+
+	double GetAmplitude(double dTime)
+	{
+		double dAmplitude = 0.0;
+		double dLifeTime = dTime - dTriggerOnTime;
+
+		if (bNoteOn)
+		{
+			if (dLifeTime <= dAttackTime)
+			{
+				// In attack Phase - approach max amplitude
+				dAmplitude = (dLifeTime / dAttackTime) * dStartAmplitude;
+			}
+
+			if (dLifeTime > dAttackTime && dLifeTime <= (dAttackTime + dDecayTime))
+			{
+				// In decay phase - reduce to sustained amplitude
+				dAmplitude = ((dLifeTime - dAttackTime) / dDecayTime) * (dSustainAmplitude - dStartAmplitude) + dStartAmplitude;
+			}
+
+			if (dLifeTime > (dAttackTime + dDecayTime))
+			{
+				// In sustain phase - dont change until note released
+				dAmplitude = dSustainAmplitude;
+			}
+		}
+		else
+		{
+			// Note has been released, so in release phase
+			dAmplitude = ((dTime - dTriggerOffTime) / dReleaseTime) * (0.0 - dSustainAmplitude) + dSustainAmplitude;
+		}
+
+		// Amplitude should not be negative
+		if (dAmplitude <= 0.0001)
+			dAmplitude = 0.0;
+
+		return dAmplitude;
+	}
+};
+
+
+atomic<double> dFrequencyOutput = 0.0;		
+sEnvelopeADSR envelope;					
+double dOctaveBaseFrequency = 110.0;
+double d12thRootOf2 = pow(2.0, 1.0 / 12.0);		// assuming western 12 notes per ocatve
+
 double makeNoise(double dTime)
 {
-	double output = 1.0 * sin(220.0 * 2 * 3.14159 * dTime);
-	if (output > 0.0)
-		return 0.2;
-	else
-		return -0.2;
+	// Mix together a little sine and square waves
+	double dOutput = envelope.GetAmplitude(dTime) *
+		(
+			+1.0 * osc(dFrequencyOutput * 0.5, dTime, OSC_TRIANGLE)
+			+ 1.0 * osc(dFrequencyOutput, dTime, OSC_SAW_ANA)
+			);
+
+	return dOutput * 0.4; 
 }
 
 int main()
 {
-	std::wcout << "Synth P1" << std::endl;
+	
+	wcout << "Sound Synthesizer" << endl << "Multiple Oscillators with Single Amplitude Envelope, No Polyphony" << endl << endl;
 
-	//Get all sound hardware
-	std::vector<std::wstring> devices = NoiseMaker<short>::Enumerate();
-										
-	//display all devices
-	for (auto d : devices)
-		std::wcout << "Found output device" << d << std::endl;																															
+	// Get all sound hardware
+	vector<wstring> devices = NoiseMaker<short>::Enumerate();
+
+	
+	for (auto d : devices) wcout << "Found Output Device: " << d << endl;
+	wcout << "Using Device: " << devices[0] << endl;
+
+	// Display a keyboard
+	wcout << endl <<
+		"|   |   |   |   |   | |   |   |   |   | |   | |   |   |   |" << endl <<
+		"|   | S |   |   | F | | G |   |   | J | | K | | L |   |   |" << endl <<
+		"|   |___|   |   |___| |___|   |   |___| |___| |___|   |   |__" << endl <<
+		"|     |     |     |     |     |     |     |     |     |     |" << endl <<
+		"|  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |" << endl <<
+		"|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|" << endl << endl;
+
+	
 	NoiseMaker<short> sound(devices[0], 44100, 1, 8, 512);
 
+	
 	sound.setUserFunction(makeNoise);
 
+	int nCurrentKey = -1;
+	bool bKeyPressed = false;
 	while (1)
 	{
-		
+		bKeyPressed = false;
+		for (int k = 0; k < 16; k++)
+		{
+			if (GetAsyncKeyState((unsigned char)("ZSXCFVGBNJMK\xbcL\xbe\xbf"[k])) & 0x8000)
+			{
+				if (nCurrentKey != k)
+				{
+					dFrequencyOutput = dOctaveBaseFrequency * pow(d12thRootOf2, k);
+					envelope.NoteOn(sound.GetTime());
+					wcout << "\rNote On : " << sound.GetTime() << "s " << dFrequencyOutput << "Hz";
+					nCurrentKey = k;
+				}
+
+				bKeyPressed = true;
+			}
+		}
+
+		if (!bKeyPressed)
+		{
+			if (nCurrentKey != -1)
+			{
+				wcout << "\rNote Off: " << sound.GetTime() << "s";
+				envelope.NoteOff(sound.GetTime());
+				nCurrentKey = -1;
+			}
+		}
 	}
 
-
-
-
-
-	return 0;	
-}								
+	return 0;
+}
